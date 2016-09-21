bc3863504a3d701e31c8de40f88700ca32a36433
Cam Hill <camkhill@gmail.com>
2016-09-21 12:44:05 -0400

    Initial commits

---
+++ MyBetsViewController.swift
@@ -0,0 +1,118 @@
+//
+//  MyBetsViewController.swift
+//  BetFriends
+//
+//  Created by Hill, Cameron on 9/19/16.
+//  Copyright © 2016 Hill, Cameron. All rights reserved.
+//
+
+import UIKit
+
+class MyBetsViewController: UIViewController {
+
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var myBetsLabel: UILabel!
+    
+    @IBOutlet weak var pendingTableView: UITableView!
+    @IBOutlet weak var activeTableView: UITableView!
+    @IBOutlet weak var completedTableView: UITableView!
+    
+    @IBOutlet weak var horizontalScrollView: UIScrollView!
+    @IBOutlet weak var newBetButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Determine screen size
+        let screenWidth = CGFloat(UIScreen.mainScreen().bounds.size.width)
+        let screenWidth3 = CGFloat(3 * UIScreen.mainScreen().bounds.size.width)
+        let screenHeight = CGFloat(UIScreen.mainScreen().bounds.size.height)
+        
+        //Set My Bets label position
+        
+        //Set Up New Bet button
+        newBetButton.frame.origin = CGPoint(x: screenWidth-60, y: screenHeight-110)
+        newBetButton.frame.size = CGSize(width: 50, height: 50)
+        
+        //Set segmentedControl state, location
+        segmentedControl.selectedSegmentIndex = 1
+        
+        //Set Margins
+        let screenTopMargin = CGFloat(100)
+        let screenBottomMargin = CGFloat(10)
+        
+        let horizontalScrollViewHeight = screenHeight-screenTopMargin-screenBottomMargin
+        let horizontalScrollViewWidth = screenWidth
+        
+        //Set the properties of scrollview
+        horizontalScrollView.frame.origin = CGPoint(x: 0, y: screenTopMargin)
+        horizontalScrollView.frame.size = CGSize(width: horizontalScrollViewWidth, height: horizontalScrollViewHeight)
+        horizontalScrollView.contentSize = CGSize(width: screenWidth3, height: horizontalScrollViewHeight)
+        horizontalScrollView.contentOffset = CGPoint(x: horizontalScrollViewWidth, y: 0)
+        
+        // Do any additional setup after loading the view.
+        
+        //Set size/positions of 3 table views
+        let sideMargins = CGFloat(10)
+        let tableViewSize = CGSize(width: horizontalScrollViewWidth-(2*sideMargins), height: horizontalScrollViewHeight)
+        
+        pendingTableView.frame.size = tableViewSize
+        activeTableView.frame.size = tableViewSize
+        completedTableView.frame.size = tableViewSize
+        
+        pendingTableView.frame.origin = CGPoint(x: sideMargins, y: 0)
+        activeTableView.frame.origin = CGPoint(x: sideMargins+screenWidth, y: 0)
+        completedTableView.frame.origin = CGPoint(x: sideMargins+(2*screenWidth), y: 0)
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //set up the segmented selector control and animation
+    @IBAction func onSelectSegment(sender: AnyObject) {
+        
+        var index = sender.selectedSegmentIndex
+        let screenWidth = CGFloat(UIScreen.mainScreen().bounds.size.width)
+        let screenWidth2 = CGFloat(2*screenWidth)
+        
+        if index == 0 {
+            UIView.animateWithDuration(0.2, animations: {
+                self.horizontalScrollView.contentOffset = CGPoint(x: 0, y: 0)
+                }, completion: { (Bool) in
+            })
+            
+        } else if index == 1 {
+            UIView.animateWithDuration(0.2, animations: {
+                self.horizontalScrollView.contentOffset = CGPoint(x: screenWidth, y: 0)
+                }, completion: { (Bool) in
+            })
+            
+        } else if index == 2 {
+            UIView.animateWithDuration(0.2, animations: {
+                self.horizontalScrollView.contentOffset = CGPoint(x: screenWidth2, y: 0)
+                }, completion: { (Bool) in
+            })
+            
+        }
+        
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
